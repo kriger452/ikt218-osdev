694ca5b99cbcacf61d324a35c055ca0abe2f05a1
shellby <clangs-k@hotmail.com>
2023-05-29 20:59:53 +0200

    Still buggy, blame it on the boogey - The Jacksons

--- group_14/src/kernel/src/main.cpp
+++ group_14/src/kernel/src/main.cpp
@@ -4,15 +4,12 @@
 
 // Custom files
 #include "include/descriptor_tables.h"
-#include "include/terminal.h"
 #include "include/interrupts.h"
 #include "include/common.h"
 #include "include/sc_to_ascii.h"
 #include "include/memory.h"
 #include "include/pit.h"
-#include "memory/malloc.c"
-#include "memory/memory.c"
-#include "memory/memutils.c"
+#include "include/terminal.h"
 
 // Define entry point in asm to prevent C++ mangling
 extern "C"{
@@ -22,7 +19,7 @@ extern "C"{
 extern uint32_t end; // This is defined in linker.ld
 
 // Overload the new operator for single object allocation
-void* operator new(std::size_t size) {
+void* operator new(size_t size) {
     return malloc(size);   // Call the C standard library function malloc() to allocate memory of the given size and return a pointer to it
 }
 
@@ -31,21 +28,12 @@ void operator delete(void* ptr) noexcept {
     free(ptr);             // Call the C standard library function free() to deallocate the memory pointed to by the given pointer
 }
 
-// Overload the new operator for array allocation
-void* operator new[](std::size_t size) {
-    return malloc(size);   // Call the C standard library function malloc() to allocate memory of the given size and return a pointer to it
-}
-
-// Overload the delete operator for array deallocation
-void operator delete[](void* ptr) noexcept {
-    free(ptr);             // Call the C standard library function free() to deallocate the memory pointed to by the given pointer
-}
-
 void kernel_main()
 {
 
-    // Initialize kernel memory manager with the end of the kernel image
-    init_kernel_memory(&end); // <------ THIS IS PART OF THE ASSIGNMENT
+    // Initialize terminal and
+	// and display a string.
+    terminal_initialize();
 
     // Initialize Global Descriptor Table (GDT)
     init_gdt();
@@ -56,42 +44,23 @@ void kernel_main()
     // Initialize Interrupt Requests (IRQs)
     init_irq();
 
+    // Initialize kernel memory manager with the end of the kernel image
+    init_kernel_memory(&end);
+    
     // Initialize Paging
-    init_paging(); // <------ THIS IS PART OF THE ASSIGNMENT
+    init_paging();
 
     // Print memory layout
-    print_memory_layout(); // <------ THIS IS PART OF THE ASSIGNMENT
+    print_memory_layout();
 
     // Setup PIT
-    init_pit();   // <------ THIS IS PART OF THE ASSIGNMENT
+    //init_pit(); 
 
     // Allocate some memory using kernel memory manager
-		// THIS IS PART OF THE ASSIGNMENT
     void* some_memory = malloc(12345); 
     void* memory2 = malloc(54321); 
     void* memory3 = malloc(13331);
-    char* memory4 = new char[1000]();
-
-....
-....
-...
-
-	// Initialize Global Descriptor Table
-	init_gdt();
-
-	// Initialize Interupt Descriptor Table
-	init_idt();
-
-	// Initialize Interupt Descriptor Table
-	init_irq();
-
-	// Initialize terminal and
-	// and display a string.
-    terminal_initialize();
-    terminal_writestring("Welcome! ");
-
-    print_memory_layout()
-
+    //char* memory4 = new char[1000]();
 
 	// Create interrupt handlers for interrupt 3 and 4
     /*
@@ -109,7 +78,7 @@ void kernel_main()
     asm volatile ("int $0x4");*/
 
     // Disable interrupts temporarily
-    /*asm volatile("sti");
+    asm volatile("sti");
 
     // Create an IRQ handler for IRQ1
     register_irq_handler(IRQ1, [](registers_t*, void*){
@@ -125,7 +94,7 @@ void kernel_main()
     // Print a message and enter an infinite loop to wait for interrupts
     // terminal_writestring("Waiting...\n");
     while(1){};
-    terminal_writestring("Done!...\n");*/
+    terminal_writestring("Done!...\n");
 }
 
 
--- group_14/src/stdlib/src/system.c
+++ group_14/src/stdlib/src/system.c
@@ -1,1 +1,126 @@
-#include "system.h"
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <limits.h>
+#include "../../kernel/src/include/terminal.h"
+
+static bool print(const char* data, size_t length) {
+	// TODO: Implement the print function according to your requirements.
+	// You can replace this with your own implementation.
+	// Return true if successful, false otherwise.
+	return false;
+}
+
+int printf(const char* restrict format, ...) {
+	va_list parameters;
+	va_start(parameters, format);
+
+	int written = 0;
+
+	while (*format != '\0') {
+		size_t maxrem = INT_MAX - written;
+
+		if (format[0] != '%' || format[1] == '%') {
+			if (format[0] == '%') {
+				format++;
+			}
+			size_t amount = 1;
+			while (format[amount] && format[amount] != '%') {
+				amount++;
+			}
+			if (maxrem < amount) {
+				// TODO: Set errno to EOVERFLOW.
+				return -1;
+			}
+			if (!print(format, amount)) {
+				return -1;
+			}
+			format += amount;
+			written += amount;
+			continue;
+		}
+
+		const char* format_begun_at = format++;
+
+		if (*format == 'c') {
+			format++;
+			char c = (char)va_arg(parameters, int /* char promotes to int */);
+			if (!maxrem) {
+				// TODO: Set errno to EOVERFLOW.
+				return -1;
+			}
+			if (!print(&c, sizeof(c))) {
+				return -1;
+			}
+			written++;
+		} else if (*format == 's') {
+			format++;
+			const char* str = va_arg(parameters, const char*);
+			size_t len = strlen(str);
+			if (maxrem < len) {
+				// TODO: Set errno to EOVERFLOW.
+				return -1;
+			}
+			if (!print(str, len)) {
+				return -1;
+			}
+			written += len;
+		} else if (*format == 'd') {
+			format++;
+			int num = va_arg(parameters, int);
+			char buffer[20];
+			int i = 0;
+			if (num == 0) {
+				buffer[i++] = '0';
+			} else if (num < 0) {
+				buffer[i++] = '-';
+				num = -num;
+			}
+			while (num != 0) {
+				buffer[i++] = num % 10 + '0';
+				num /= 10;
+			}
+			while (i > 0) {
+				if (maxrem < 1) {
+					// TODO: Set errno to EOVERFLOW.
+					return -1;
+				}
+				if (!print(&buffer[--i], 1)) {
+					return -1;
+				}
+				written++;
+			}
+		} else if (*format == 'x') {
+			format++;
+			unsigned int num = va_arg(parameters, unsigned int);
+			char buffer[20];
+			int i = 0;
+			if (num == 0) {
+				buffer[i++] = '0';
+			}
+			while (num != 0) {
+                int rem = num % 16;
+                if (rem < 10) {
+                    buffer[i++] = rem + '0';
+                } else {
+                    buffer[i++] = rem - 10 + 'a';
+                }
+                num /= 16;
+            }
+            while (i > 0) {
+                if (maxrem < 1) {
+                    // TODO: Set errno to EOVERFLOW.
+                    return -1;
+                }
+                if (!print(&buffer[--i], 1)) {
+                    return -1;
+                }
+                written++;
+            }
+        }
+    }
+    return written;
+}
+
+
